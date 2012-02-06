5cf52a151a9a687756f9194f87b23c884c6b4575
Dustin Sallings <dustin@spy.net>
2012-02-05 22:56:36 -0800

    Split out http work.

--- client/main.go
+++ client/main.go
@@ -1,68 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
-	"errors"
 	"flag"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
 )
-
-type FileData struct {
-	Size  int64  `json:"size"`
-	Mode  int32  `json:"mode"`
-	Mtime int64  `json:"mtime"`
-	Hash  uint64 `json:"hash,omitempty"`
-	Dest  string `json:"linkdest,omitempty"`
-}
-
-func decodeURL(u string) (map[string]FileData, error) {
-	rv := map[string]FileData{}
-
-	resp, err := http.Get(u)
-	if err != nil {
-		return rv, err
-	}
-	if resp.StatusCode != 200 {
-		return rv, errors.New(fmt.Sprintf("Error httping: %v", resp.Status))
-	}
-	defer resp.Body.Close()
-	r := bufio.NewReader(resp.Body)
-
-	d := json.NewDecoder(r)
-
-	done := false
-	for !done {
-		type fdata struct {
-			Name  string `json:"name"`
-			Size  int64  `json:"size"`
-			Mode  int32  `json:"mode"`
-			Mtime int64  `json:"mtime"`
-			Hash  uint64 `json:"hash,omitempty"`
-			Dest  string `json:"linkdest,omitempty"`
-		}
-		fd := fdata{}
-		err = d.Decode(&fd)
-		switch err {
-		default:
-			return rv, errors.New(fmt.Sprintf("Error decoding: %v", err))
-		case nil:
-			rv[fd.Name] = FileData{
-				Size:  fd.Size,
-				Mode:  fd.Mode,
-				Mtime: fd.Mtime,
-				Hash:  fd.Hash,
-				Dest:  fd.Dest,
-			}
-		case io.EOF:
-			done = true
-		}
-	}
-	return rv, nil
-}
 
 func dbFromURL(u, path string) error {
 	data, err := decodeURL(u)
