eb1ea11fbe169a471bfd8a98abb1d10036c8c422
Dustin Sallings <dustin@spy.net>
2014-06-15 10:50:09 -0700

    Wrote tests for decodeURL and simplified it greatly

--- client/http.go
+++ client/http.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -33,41 +32,21 @@ func (c *bitfogClient) decodeURL(u string) (map[string]bitfog.FileData, error) {
 		return rv, fmt.Errorf("error httping: %v", resp.Status)
 	}
 	defer resp.Body.Close()
-	r := bufio.NewReader(resp.Body)
 
-	d := json.NewDecoder(r)
+	d := json.NewDecoder(resp.Body)
 
-	done := false
-	for !done {
-		type fdata struct {
-			Name  string `json:"name"`
-			Size  int64  `json:"size"`
-			Mode  int32  `json:"mode"`
-			Mtime int64  `json:"mtime"`
-			Hash  uint64 `json:"hash,omitempty"`
-			Dest  string `json:"linkdest,omitempty"`
-		}
-		fd := fdata{}
+	for {
+		fd := bitfog.FileData{}
 		err = d.Decode(&fd)
 		switch err {
 		default:
 			return rv, fmt.Errorf("error decoding: %v", err)
 		case nil:
-			if verbose {
-				fmt.Printf(" got %#v\n", fd)
-			}
-			rv[fd.Name] = bitfog.FileData{
-				Size:  fd.Size,
-				Mode:  fd.Mode,
-				Mtime: fd.Mtime,
-				Hash:  fd.Hash,
-				Dest:  fd.Dest,
-			}
+			rv[fd.Name] = fd
 		case io.EOF:
-			done = true
+			return rv, nil
 		}
 	}
-	return rv, nil
 }
 
 func (c *bitfogClient) downloadFile(src, dest string) error {
---
+++ client/http_test.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/dustin/bitfog"
+)
+
+type constantTransport struct {
+	status int
+	body   []byte
+}
+
+var errNotInitialized = errors.New("no transport")
+
+func (c *constantTransport) RoundTrip(*http.Request) (*http.Response, error) {
+	if c == nil {
+		return nil, errNotInitialized
+	}
+	return &http.Response{
+		StatusCode: c.status,
+		Status:     http.StatusText(c.status),
+		Body:       ioutil.NopCloser(bytes.NewReader(c.body)),
+	}, nil
+}
+
+func fakeClient(status int, body string) *bitfogClient {
+	return &bitfogClient{&http.Client{Transport: &constantTransport{status, []byte(body)}}}
+}
+
+func brokenClient() *bitfogClient {
+	return &bitfogClient{&http.Client{Transport: (*constantTransport)(nil)}}
+}
+
+func TestDecodeFail(t *testing.T) {
+	c := brokenClient()
+	rv, err := c.decodeURL("http://whatever/")
+	if err == nil {
+		t.Errorf("Expected failure, got %v", rv)
+	}
+
+	c = fakeClient(500, "Broken")
+	rv, err = c.decodeURL("http://whatever/")
+	if err == nil {
+		t.Errorf("Expected failure, got %v", rv)
+	}
+}
+
+func TestDecodeSuccess(t *testing.T) {
+	res := `{"name": "a", "size": 37665, "mode": 644, "mtime": 1402853551, "hash": 90018}
+{"name": "b", "size": 21866, "mode": 644, "mtime": 1402853551, "hash": 62130}
+{"name": "c", "size": 75648, "mode": 644, "mtime": 1402853551, "hash": 51301, "linkdest": "a"}`
+
+	c := fakeClient(200, res)
+	rv, err := c.decodeURL("http://whatever/")
+	if err != nil {
+		t.Fatalf("Error decoding:  %v", err)
+	}
+	if len(rv) != 3 {
+		t.Fatalf("Expected three results, got %v", rv)
+	}
+	exp := map[string]bitfog.FileData{
+		"a": bitfog.FileData{Name: "a", Size: 37665, Mode: 644, Mtime: 1402853551, Hash: 90018},
+		"b": bitfog.FileData{Name: "b", Size: 21866, Mode: 644, Mtime: 1402853551, Hash: 62130},
+		"c": bitfog.FileData{Name: "c", Size: 75648, Mode: 644, Mtime: 1402853551, Hash: 51301, Dest: "a"},
+	}
+
+	for k, v := range exp {
+		got := rv[k]
+		if v != got {
+			t.Errorf("Wrong at %q: wanted %#v, got %#v", k, v, got)
+		}
+	}
+}
+
+func TestDecodePartialSuccess(t *testing.T) {
+	res := `{"name": "a", "size": 37665, "mode": 644, "mtime": 1402853551, "hash": 90018} blah`
+
+	c := fakeClient(200, res)
+	rv, err := c.decodeURL("http://whatever/")
+	if err == nil {
+		t.Fatalf("Expected an error, but didn't get one:  %v", err)
+	}
+	if len(rv) != 1 {
+		t.Fatalf("Expected one result, got %v", rv)
+	}
+	exp := map[string]bitfog.FileData{
+		"a": bitfog.FileData{Name: "a", Size: 37665, Mode: 644, Mtime: 1402853551, Hash: 90018},
+	}
+
+	for k, v := range exp {
+		got := rv[k]
+		if v != got {
+			t.Errorf("Wrong at %q: wanted %#v, got %#v", k, v, got)
+		}
+	}
+}
